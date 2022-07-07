466054dd4c10d9c7c51574fb19a8c4dd165f365f
Moon Juhan <codemike@naver.com>
2022-07-07 22:12:52 +0900

    feat: Add switch down func

--- arduino_foot_switch.ino
+++ arduino_foot_switch.ino
@@ -1,15 +1,33 @@
-#define PIN 5
+// #include <Keyboard.h>
 
-void setup() {
+#define PIN 5
+#define SWITCH_DOWN 1
+
+void setup()
+{
   Serial.begin(9600);
   pinMode(PIN, INPUT);
 }
 
-void loop() {
-  int buttonState01 = digitalRead(PINPIN);
-  
-  Serial.print(buttonState01);
-  Serial.print("\n"); 
-  
-  delay(1);
+int buttonState;
+
+void loop()
+{
+  if (buttonState != digitalRead(PIN))
+  {
+    print(buttonState == SWITCH_DOWN);
+  }
+
+  buttonState = digitalRead(PIN);
+
+  delay(100);
+}
+
+void print(bool isPrint)
+{
+  if (isPrint == SWITCH_DOWN)
+  {
+    Serial.println("PRINT");
+    //  Keyboard.press('a');
+  }
 }
