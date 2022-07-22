024421ba4f40d33f4026cc3c154fee83350cdd4b
Moon Juhan <codemike@naver.com>
2022-07-22 21:41:51 +0900

    feat: Add keyboard func

--- arduino_foot_switch.ino
+++ arduino_foot_switch.ino
@@ -1,6 +1,6 @@
-// #include <Keyboard.h>
+#include <Keyboard.h>
 
-#define PIN 5
+#define PIN 10
 #define SWITCH_DOWN 1
 
 void setup()
@@ -28,6 +28,12 @@ void print(bool isPrint)
   if (isPrint == SWITCH_DOWN)
   {
     Serial.println("PRINT");
-    //  Keyboard.press('a');
+    Keyboard.press('c');
+    Keyboard.press('o');
+    Keyboard.press('n');
+    Keyboard.press('s');
+    Keyboard.press('t');
+    Keyboard.press(' ');
+    Keyboard.releaseAll();
   }
 }
